492503f5d17becf67cb7b4d344e9f3a31935111e
Lydia <lydia@twobytwo.app>
2024-05-03 19:57:39 +0100

    works now

---
+++ src/app.js
@@ -0,0 +1,24 @@
+// app.js
+const postgres = require('postgres');
+require('dotenv').config();
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+async function getPgVersion() {
+  const result = await sql`select * from playing_with_neon`;
+  console.log(result);
+}
+
+getPgVersion();
